21a58e7aff175d6b68d6b5d82c5c4e7fde0f654c
Aphaki <kiapha1108@gmail.com>
2023-03-29 17:08:37 +0900

    seague complete

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Model/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Sy Lee on 2023/03/29.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let registerSeagueId = "GoToRegisterView"
+    static let loginSeagueId = "GoToLoginView"
+    static let registerToChatSeagueId = "GoToChatViewOne"
+    static let loginToChatSeagueId = "GoToChatViewTwo"
+}
